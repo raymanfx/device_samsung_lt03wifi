8055d7c07d78f0ce4fc7f108dc32341db76ebb3c
Christopher N. Hesse <raymanfx@gmail.com>
2015-01-22 14:01:23 +0100

    Revert "lt03wifi: align touchscreen power handling to manta"
    This reverts commit 0858c4a2dc693680f0ade9b0df5dc817f3bdb9d0.
    
    Conflicts:
    	power/power.c
    	rootdir/etc/ueventd.universal5420.rc

--- power/power.c
+++ power/power.c
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <dirent.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
@@ -28,6 +27,7 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
+#define TSP_POWER "/sys/class/input/input1/enabled"
 #define BOOSTPULSE_PATH "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
 #define BOOST_PATH "/sys/devices/system/cpu/cpufreq/interactive/boost"
 //BOOST_PULSE_DURATION and BOOT_PULSE_DURATION_STR should always be in sync
@@ -42,14 +42,13 @@ struct universal5420_power_module {
     pthread_mutex_t lock;
     int boostpulse_fd;
     int boostpulse_warned;
-    const char *touchscreen_power_path;
 };
 
 static unsigned int vsync_count;
 static struct timespec last_touch_boost;
 static bool touch_boost;
 
-static void sysfs_write(const char *path, char *s) {
+static void sysfs_write(char *path, char *s) {
     char buf[80];
     int len;
     int fd = open(path, O_WRONLY);
@@ -69,47 +68,8 @@ static void sysfs_write(const char *path, char *s) {
     close(fd);
 }
 
-static void init_touchscreen_power_path(struct universal5420_power_module *universal5420)
-{
-    char buf[80];
-    const char dir[] = "/sys/class/input/input1/enabled";
-    const char filename[] = "enabled";
-    DIR *d;
-    struct dirent *de;
-    char *path;
-    int pathsize;
-
-    d = opendir(dir);
-    if (d == NULL) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error opening directory %s: %s\n", dir, buf);
-        return;
-    }
-    while ((de = readdir(d)) != NULL) {
-        if (strncmp("input", de->d_name, 5) == 0) {
-            pathsize = strlen(dir) + strlen(de->d_name) + sizeof(filename) + 2;
-            path = malloc(pathsize);
-            if (path == NULL) {
-                strerror_r(errno, buf, sizeof(buf));
-                ALOGE("Out of memory: %s\n", buf);
-                return;
-            }
-            snprintf(path, pathsize, "%s/%s/%s", dir, de->d_name, filename);
-            universal5420->touchscreen_power_path = path;
-            goto done;
-        }
-    }
-    ALOGE("Error failed to find input dir in %s\n", dir);
-done:
-    closedir(d);
-}
-
 static void power_init(struct power_module *module)
 {
-    struct universal5420_power_module *universal5420 = (struct universal5420_power_module *) module;
-    struct dirent **namelist;
-    int n;
-
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
                 "20000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_slack",
@@ -126,17 +86,13 @@ static void power_init(struct power_module *module)
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boostpulse_duration",
                 BOOST_PULSE_DURATION_STR);
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/io_is_busy", "1");
-    
-    init_touchscreen_power_path(universal5420);
 }
 
 static void power_set_interactive(struct power_module *module, int on)
 {
-    struct universal5420_power_module *universal5420 = (struct universal5420_power_module *) module;
-    
     ALOGV("power_set_interactive: %d\n", on);
     
-    sysfs_write(universal5420->touchscreen_power_path, on ? "1" : "0");
+    sysfs_write(TSP_POWER, on ? "1" : "0");
     
     ALOGV("power_set_interactive: %d done\n", on);
 }
